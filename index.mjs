ca180abc7c8bb66698f9a461165dc50118034462
plaury <94334028+plaury@users.noreply.github.com>
2024-04-14 17:51:36 -0400

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,33 +1,12 @@
 import createServer from '@tomphttp/bare-server-node';
 import http from 'http';
 import nodeStatic from 'node-static';
-//const cors = require('cors');
-//const frameguard = require('frameguard') //imports iframe block
-//const config = require('./config.json')
 const port = process.env.PORT || 8080;
-//const Corrosion = require('./lib/server')
-//const btoa = e => new Buffer.from(e).toString("base64")
-
-/*const proxy = new Corrosion({
-  prefix: "/beta/",
-  codec: "base64",
-  title: "Classes",
-  forceHttps: true,
-  requestMiddleware: [
-  Corrosion.middleware.blacklist([
-    'accounts.google.com'
-  ], 'This page is unavailable.'),
-]
-});*/
-
-//proxy.bundleScripts();
 
 const bare =  createServer('/bare/');
 const serve = new nodeStatic.Server('main/');
 
 const server = http.createServer();
-
-//app.use(frameguard({ action: 'SAMEORIGIN' })) //blocks iframing this site
 
 server.on('request', (req, res) => {
   if (bare.shouldRoute(req)) {
@@ -44,34 +23,6 @@ server.on('upgrade', (req, socket, head) => {
     socket.end();
   }
 });
-
-/*app.get('/', function(req, res){
-  res.sendFile('index.html', {root: './main'});
-});
-
-app.use('/g/', function(req, res, next){
-  res.sendFile('gams.html', {root: './main'});
-});
-
-app.get('/js/go.js', function(req, res){
-  res.sendFile('go.js', {root: './main/js/'});
-});
-
-app.get('/favicon.ico', function(req, res){
-  res.sendFile('favicon.ico', {root: './main/img/'});
-});
-
-app.use(express.static('./main', {
-  extensions: ['html']
-}));*/
-
-/*app.use(function (req, res) {
-  if (req.url.startsWith(proxy.prefix)) {
-    proxy.request(req,res);
-  } else {
-    res.status(404).sendFile('404.html', {root: './main'});
-  }
-}).post('*', (req, res) => {})*/
 
 server.listen({
   port: port,
